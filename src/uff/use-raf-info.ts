534b08145ae6d889aef050dbdd269a30dbeb123b
Ajit <zlksnkwork@gmail.com>
2022-12-17 12:31:39 +0530

    cool

--- src/uff/use-raf-info.ts
+++ src/uff/use-raf-info.ts
@@ -1,5 +1,7 @@
 import { raf } from "@react-spring/shared";
 
+import { clamp } from "./clamp";
+
 let lastT = Math.round(raf.now());
 const info = {
   timeDelta: 0,
@@ -7,7 +9,9 @@ const info = {
 
 raf(() => {
   const now = Math.round(raf.now());
-  info.timeDelta = now - lastT;
+  // Framer Motion clamps timedelta for safe measure. I've done it for the same reason
+  // ref. https://github.com/framer/motion/blob/bb7ee043bff53c2a817c599805d6d3e71308b640/packages/framer-motion/src/frameloop/index.ts#L50
+  info.timeDelta = clamp(1, 40, now - lastT);
   lastT = now;
   return true;
 });
