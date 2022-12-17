b5cfc14e161bed9c25773257b5ccd34d1bd5a534
Ajit <zlksnkwork@gmail.com>
2022-12-17 13:14:30 +0530

    add progress

--- src/uff/to.ts
+++ src/uff/to.ts
@@ -4,7 +4,7 @@ type Range = [min: number, max: number];
 // `transform`, `interp1d` or just `interp` may or may not be a better name
 export function to([inpMin, inpMax]: Range, [outMin, outMax]: Range, x: number) {
   const inpRange = inpMax - inpMin;
-  if (inpRange === 0) return outMin;
+  if (inpRange === 0) return outMax;
 
   return ((x - inpMin) * (outMax - outMin)) / (inpMax - inpMin) + outMin;
 }
